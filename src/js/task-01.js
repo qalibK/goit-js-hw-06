b2a4a95721c3166904b858f0eddd71d096528be8
qalibK <bvomusak01@gmail.com>
2023-11-12 21:01:49 +0100

    feat(task-done)!: doned 3 tasks
    task 1 ---> done (js)
    task 2 ---> done (js)
    task 3 ---> done (js)

---
+++ src/js/task-01.js
@@ -0,0 +1,15 @@
+const categoriesListEl = document.querySelector("ul#categories");
+
+const categoryItemsEl = categoriesListEl.querySelectorAll("li.item");
+
+console.log(`Number of categories: ${categoryItemsEl.length}`);
+
+categoryItemsEl.forEach((categoryItem) => {
+  const categoryTitle = categoryItem.querySelector("h2").textContent;
+
+  const categoryElement = categoryItem.querySelectorAll("ul > li");
+
+  console.log(`
+  Category: ${categoryTitle}   
+  Elements: ${categoryElement.length}`);
+});
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,20 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+const ingredientPickerOptions = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientListEl = document.querySelector("#ingredients");
+
+const ingredientElements = ingredientPickerOptions.map(option => {
+  const listItemEl = document.createElement("li");
+  listItemEl.textContent = option;
+  listItemEl.classList.add("item");
+
+  return listItemEl;
+});
+
+ingredientListEl.append(...ingredientElements);
